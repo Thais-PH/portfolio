6367b2f81cae3b91fefa08c9f0104687fea11f19
Thais-PH <thais.prinborgne-hervouet@hotmail.fr>
2026-01-22 16:12:33 +0100

    correction instrumentation.ts

--- instrumentation.ts
+++ instrumentation.ts
@@ -2,18 +2,18 @@ export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     const { NodeSDK } = await import('@opentelemetry/sdk-node');
     const { OTLPTraceExporter } = await import('@opentelemetry/exporter-trace-otlp-http');
-    const { Resource } = await import('@opentelemetry/resources');
-    const { SEMRESATTRS_SERVICE_NAME } = await import('@opentelemetry/semantic-conventions');
+    const { resourceFromAttributes } = await import('@opentelemetry/resources');
+    const { ATTR_SERVICE_NAME } = await import('@opentelemetry/semantic-conventions');
     const { getNodeAutoInstrumentations } = await import('@opentelemetry/auto-instrumentations-node');
 
     const sdk = new NodeSDK({
-      resource: new Resource({
-        [SEMRESATTRS_SERVICE_NAME]: 'portfolio-thais-backend',
+      resource: resourceFromAttributes({
+        [ATTR_SERVICE_NAME]: 'portfolio-thais-backend',
       }),
       traceExporter: new OTLPTraceExporter({
         url: `${process.env.OTEL_EXPORTER_OTLP_ENDPOINT}/v1/traces`,
         headers: {
-            Authorization: process.env.OTEL_EXPORTER_OTLP_HEADERS,
+            Authorization: process.env.OTEL_EXPORTER_OTLP_HEADERS || '',
         }
       }),
       instrumentations: [getNodeAutoInstrumentations()],
